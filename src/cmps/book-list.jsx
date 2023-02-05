c724bb5cc040e830376f104c82fec95b4f4fbd03
Liron Kruchinin <lironkruch@gmail.com>
2023-02-05 13:51:59 +0200

    imported scss and added wishlist books

--- src/cmps/book-list.jsx
+++ src/cmps/book-list.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BookPreview } from "./book-preview";
+import { WishlistBooks } from "./wishlist-books";
 
 
 export const BookList = () => {
   return <div className="book-list">
     <BookPreview />
-  </div>;
-};
+    <WishlistBooks />
+  </div>
+}
