e508eac505e542dfe597633506e87a4e95ed4725
Liron Kruchinin <lironkruch@gmail.com>
2023-02-05 16:40:18 +0200

    fixed paging bug

--- src/views/book-index.jsx
+++ src/views/book-index.jsx
@@ -9,30 +9,28 @@ export const BookIndex = () => {
   const [book, setBook] = useState({})
   const [books, setBooks] = useState([])
 
-
   useEffect(() => {
     loadBooks()
   }, [])
 
   useEffect(() => {
-    bookPaging(1)
+    bookPaging()
+  }, [books, pageIdx])
 
-  }, [books])
 
   async function loadBooks() {
     const books = await bookService.query();
     setBooks(books)
   }
 
-  function bookPaging(diff) {
+  function bookPaging() {
     const currBook = books.slice(pageIdx * PAGE_SIZE, (pageIdx + 1) * PAGE_SIZE)
     setBook(...currBook)
-    setPageIdx(pageIdx + diff || 0)
   }
 
   function onChangePage(diff) {
     setPageIdx(pageIdx + (diff))
-    bookPaging(diff)
+    bookPaging()
   }
 
   return (
