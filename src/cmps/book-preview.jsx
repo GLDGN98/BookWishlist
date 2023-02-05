7087f88bbb5e3c05a4b119688fd36379e199599f
Gilad Dagan <giladagan53@gmail.com>
2023-02-05 13:58:49 +0200

    added basic paging

--- src/cmps/book-preview.jsx
+++ src/cmps/book-preview.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export const BookPreview = ({book}) => {
   return <div className="book-preview">
+    <div>
     <h3>{book.title}</h3>
+    <input type="checkbox" name="" id="" />
+    </div>
     <hr />
     <p>{book.author}</p>
     <p>{book.description}</p>
