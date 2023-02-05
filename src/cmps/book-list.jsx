5cd170c0736dab1c751b0e8f639c938679487181
Liron Kruchinin <lironkruch@gmail.com>
2023-02-05 13:58:53 +0200

    changed scss structure

--- src/cmps/book-list.jsx
+++ src/cmps/book-list.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { BookPreview } from "./book-preview";
-import { WishlistBooks } from "./wishlist-books";
 
 
-export const BookList = ({books}) => {
+export const BookList = ({ books }) => {
   return <ul className="book-list">
     {books?.map(book => {
-        return (
-            <li>
-                <BookPreview book={book} />
-            </li>
-        )
+      return (
+        <li>
+          <BookPreview book={book} />
+        </li>
+      )
     })}
   </ul>;
 };
--- src/views/book-index.jsx
+++ src/views/book-index.jsx
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { BookList } from "../cmps/book-list";
+import { WishlistBooks } from "../cmps/wishlist-books";
 import { bookService } from "../services/book.service";
 
 export const BookIndex = () => {
 
   const [books, setBooks] = useState([])
 
-useEffect(() => {
-  loadBooks()
-},[])
+  useEffect(() => {
+    loadBooks()
+  }, [])
 
 
- function loadBooks() {  
-bookService.query().then(setBooks)
-   
-}
+  function loadBooks() {
+    bookService.query().then(setBooks)
+
+  }
 
 
   return <div className="book-index">
     <BookList books={books} />
+    <WishlistBooks />
   </div>;
 };
